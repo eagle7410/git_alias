7de8dc40583edb6ab334692999a6b921ed801eee
Igor <verycooleagle@gmail.com>
2019-02-24 01:56:34 +0200

    Create system_alias.sh

---
+++ system_alias.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+echo "
+alias gs='git status '
+alias ga='git add '
+alias gb='git branch '
+alias gc='git commit'
+alias gd='git diff'
+alias go='git checkout '
+" >> ~/.profile
